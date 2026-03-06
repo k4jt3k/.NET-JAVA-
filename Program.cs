a4b72983340abce9f94b139e9177a173e25dcef3
student <student@DESKTOP-I3OGHQQ>
2026-03-06 08:11:24 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz gra = new FizzBuzz(20);
+        }
+    }
+
+    internal class FizzBuzz
+    {
+        public FizzBuzz(int zakres)
+        {
+            for (int n = 1; n <= zakres; n++)
+            {
+                if (n % 3 ==0 && n % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
